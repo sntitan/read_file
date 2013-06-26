da73623b36d7e7e8fa586ba6a1f160ad3d88bafa
sntitan <sntitan@gmail.com>
2013-06-26 23:02:58 +0800

    add a new empty file

---
+++ read_in_file.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+
+int file_into_stru(FILE* pfile)
+{
+    return 0;
+}
+
+
+int main(int argc, char* argv[])
+{
+    FILE* pfile = NULL;
+    if(argc != 2)
+    {
+	printf("need a file name!\n");
+        return -1;
+    }
+    if(argv[1] == NULL)
+    {
+        return -1;
+    }
+    printf("read in file %s\n",argv[1]);
+    pfile = fopen(argv[1], "rb");
+
+    file_into_stru(pfile);
+
+    fclose(pfile);
+    printf("read over!\n");
+    return 0;
+    
+}
